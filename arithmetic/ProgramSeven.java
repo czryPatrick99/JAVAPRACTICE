2af64dcce0db447e6eb6139c57db7b76cd549a79
czryPatrick99 <poudelpratik2025@gmail.com>
2026-03-23 20:05:18 +0545

    Idea of Arithemetic operation in Java

---
+++ arithmetic/ProgramSeven.java
@@ -0,0 +1,15 @@
+package arithmetic;
+
+public class ProgramSeven {
+    public static void main(String [] args){
+        int number1 = 109;
+        int number2 = 19;
+        int number3 = 99;
+        System.out.println("Number 1 : " + number1 + " Number 2 : " + number2 + " Number 3 : " + number3);
+
+        System.out.println("The sum of the three numbers is: " + (number1 + number2 + number3));
+        System.out.println("The product of the three numbers is: " + (number1 * number2 * number3));
+        System.out.println("The average of the three numbers is: " + ((number1 + number2 + number3) / 3));
+        
+    }
+}
